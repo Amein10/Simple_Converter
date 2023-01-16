90ca29a276595ca21e697408972d6fee68bd4738
Amein10 <18572996+Amein10@users.noreply.github.com>
2023-01-16 19:13:34 +0100

    Add files via upload

---
+++ H1GPDag1_Converter/H1GPDag1_Converter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double miles, km;
+            Console.Write("Enter Value of Kilometer: ");
+            km = Convert.ToDouble(Console.ReadLine());
+            miles = km * 0.62137;
+            Console.WriteLine("Miles = " + miles);
+        }
+    }   
+}
